7df92f3845169a61e6c2f14a22e6c70462a41e9e
nhanasi <navihansi@gmail.com>
2025-08-29 13:19:43 -0400

    Update mtlsUtils.h

--- rfcMgr/mtlsUtils.h
+++ rfcMgr/mtlsUtils.h
@@ -57,9 +57,10 @@ int isStateRedSupported(void);
 int isInStateRed(void);
 #endif
 int getMtlscert(MtlsAuth_t *sec);
+#if defined(RDKB_SUPPORT)
 std::string getErouterMac();
 std::string geteCMMac();
-
+#endif
 #ifdef __cplusplus
 }
 #endif
