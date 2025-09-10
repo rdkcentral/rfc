1461f434ba765560790a97f590bcecea477461d4
nhanasi <navihansi@gmail.com>
2025-09-10 15:03:43 -0400

    Update mtlsUtils.h

--- rfcMgr/mtlsUtils.h
+++ rfcMgr/mtlsUtils.h
@@ -32,9 +32,6 @@ extern "C" {
 #include <system_utils.h>
 #include <urlHelper.h>
 
-#define MTLS_SUCCESS 1
-#define MTLS_FAILURE -1
-
 #ifdef LIBRDKCERTSELECTOR	
 #include "rdkcertselector.h"
 
@@ -49,6 +46,8 @@ typedef enum {
 
 MtlsAuthStatus getMtlscert(MtlsAuth_t *sec, rdkcertselector_h* pthisCertSel);
 #else
+#define MTLS_SUCCESS 1
+#define MTLS_FAILURE -1
 int getMtlscert(MtlsAuth_t *sec);
 #endif
 
