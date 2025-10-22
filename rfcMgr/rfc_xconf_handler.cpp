a38a00b8980ab07f7a4863bcc822955f8dce1605
KTirumalaSrihari <102281309+KTirumalaSrihari@users.noreply.github.com>
2025-10-22 20:31:22 +0530

    Update rfc_xconf_handler.cpp

--- rfcMgr/rfc_xconf_handler.cpp
+++ rfcMgr/rfc_xconf_handler.cpp
@@ -46,7 +46,7 @@ extern "C" {
 #define MTLS_FAILURE -1
 #endif
 
-inline bool RuntimeFeatureControlProcessor:: Debug_Services_Enabled(bool DbgBuild, BUILDTYPE eBuildType, bool dbgServices, eDeviceType deviceType) { 
+bool RuntimeFeatureControlProcessor:: Debug_Services_Enabled(bool DbgBuild, BUILDTYPE eBuildType, bool dbgServices, eDeviceType deviceType) { 
      return (DbgBuild && (eBuildType == ePROD) && dbgServices && (deviceType == DEVICE_TYPE_TEST)) || (eBuildType == eDEV);
 }
 
