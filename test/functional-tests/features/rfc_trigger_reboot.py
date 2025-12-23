f609bb466cdeeda359364759d69c42533a007718
madhubabutt <114217841+madhubabutt@users.noreply.github.com>
2025-12-24 01:55:06 +0530

    RDKEMW-11615 : Avoid Triggering reboot for the AccountID Unknown scenarios Added L2 (#133)
    Co-authored-by: mtirum011 <madhubabu_tirumala@comcast.com>

---
+++ test/functional-tests/features/rfc_trigger_reboot.py
@@ -0,0 +1,36 @@
+####################################################################################
+# If not stated otherwise in this file or this component's Licenses file the
+# following copyright and licenses apply:
+#
+# Copyright 2024 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####################################################################################
+
+Feature: Verify RFC request parameters
+
+  Scenario: RFC GET request is sent with correct parameters
+    Given the mockxconf server is running
+    When a RFC curl request is made to "https://mockxconf:50053/featureControl/getSettings?" with:
+      | method | GET |
+      | query  | estbMacAddress=01%3A23%3A45%3A67%3A89%3Aab&firmwareVersion=T2_Container_0.0.0&env=prod&model=L2CNTR&manufacturer=&controllerId=2504&channelMapId=2345&VodId=15660&partnerId=global&osClass=&accountId=Unknown&Experience=X1&version=2 |
+    Then the mockxconf server should have received a request to "https://mockxconf:50053/featureControl/getSettings?"
+    And the request method should be "GET"
+    And the request query parameters should contain:
+	| accountId       | Unknown            |
+    And the response from the xconf server and the account id should be Valid
+	| accountId       | 3064488088886635972|
+    And a message "EMPTY value for Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.AccountInfo.AccountID is rejected" should be logged
+    And a message "Checking Config Value changed for tr181 param" should be logged
+    And a message "RFC: Comparing Xconfvalue='3064488088886635972' with Unknown" should be logged
+    And a message "AccountId is Valid 3064488088886635972, Updating the device Database" should be logged
