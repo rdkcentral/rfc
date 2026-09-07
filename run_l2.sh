c4cd25599f40afe9f8496777921cc59ef027144b
KTirumalaSrihari <102281309+KTirumalaSrihari@users.noreply.github.com>
2026-09-07 19:00:42 +0530

    Update run_l2.sh

--- run_l2.sh
+++ run_l2.sh
@@ -36,6 +36,7 @@ pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rfc_in
 
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rfc_xconf_communication_success.json test/functional-tests/tests/test_rfc_xconf_communication.py
 
+
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rfc_setget_param.json test/functional-tests/tests/test_rfc_setget_param.py
 
 pytest --json-report --json-report-summary --json-report-file $RESULT_DIR/rfc_tr181_setget_local_param.json test/functional-tests/tests/test_rfc_tr181_setget_local_param.py
